f6801b2cab91400c4befa9e46064ac8c963cbe86
hanweiwu <hanwei_wu@163.com>
2021-11-02 21:32:45 +0800

    登陆验证码改用本地随机图片

--- src/main/webapp/Js/disk/longbow.js
+++ src/main/webapp/Js/disk/longbow.js
@@ -166,7 +166,8 @@
             loadCount = 0;
             that.text.removeClass('text-danger');
             if ($.isFunction(that.options.setSrc)) src = that.options.setSrc();
-            if (!src || src === '') src = 'https://picsum.photos/' + that.options.width + '/' + that.options.height + '/?image=' + Math.round(Math.random() * 20);
+            // if (!src || src === '') src = 'https://picsum.photos/' + that.options.width + '/' + that.options.height + '/?image=' + Math.round(Math.random() * 20);
+            src = "../Js/disk/img/" + Math.floor(Math.random() * 49) + '.jpg';
             if (isIE) { // IE浏览器无法通过img.crossOrigin跨域，使用ajax获取图片blob然后转为dataURL显示
                 var xhr = new XMLHttpRequest()
                 xhr.onloadend = function (e) {
