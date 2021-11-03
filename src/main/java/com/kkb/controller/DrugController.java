8f64a67271b58634e570e7843a0b77ebb50ca650
guguofu <3165177264@qq.com>
2021-11-04 00:25:16 +0800

    完善药品添加药品的后端校验，还未加上前后端校验

--- src/main/java/com/kkb/controller/DrugController.java
+++ src/main/java/com/kkb/controller/DrugController.java
@@ -1,14 +1,20 @@
 package com.kkb.controller;
 
 import com.kkb.pojo.Drug;
+import com.kkb.service.AccessService;
 import com.kkb.service.DrugService;
+import com.kkb.utils.ValidUtil;
 import com.kkb.vo.AjaxResultVo;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Date;
 
 /**
  * show 药品业务的控制器
@@ -24,6 +30,9 @@ public class DrugController {
     @Resource
     private DrugService drugService;
 
+    @Resource
+    AccessService accessService;
+
 
     /**
      * 新增药品
@@ -31,13 +40,22 @@ public class DrugController {
      * @return 新增结果
      */
     @RequestMapping(value = "",method = RequestMethod.POST)
-    public AjaxResultVo addDrug(Drug drug){
+    public AjaxResultVo addDrug(@Valid Drug drug, HttpServletRequest request){
         // 药品编码唯一性判断
         Drug byId = drugService.findDrugById(drug.getDrId());
+        // 该编码已经存在了
         if (byId!=null){
             return new AjaxResultVo(400,"药品编码不唯一,请重新输入");
         }
-        // 可加图片检查，其他检查在实体类中加上了
+        // 设置创建人和创建时间
+        try {
+            drug.setuId(accessService.paseUserMessage(request).getuId());
+            drug.setDrCreateTime(new Date());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("获取当前用户失败");
+            return new AjaxResultVo(403,"用户未登录");
+        }
         // 保存药品
         int i = drugService.addDrug(drug);
         if (i==1){
---
+++ src/main/java/com/kkb/utils/ValidUtil.java
@@ -0,0 +1,48 @@
+package com.kkb.utils;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+/**
+ * show 用于校验参数的工具类
+ *
+ * @author guguofu
+ * @since 2021/11/3
+ */
+public class ValidUtil {
+
+    // 从绑定的参数中返回错误的信息字符串。
+    public static String getErrorMessage(BindingResult result){
+        boolean hasError = result.hasErrors();
+        if (hasError){
+            StringBuilder errorMessage = new StringBuilder();
+            List<FieldError> errors = result.getFieldErrors();
+            for (FieldError fe : errors){
+                errorMessage.append(fe.getField());
+                errorMessage.append(":");
+                errorMessage.append(fe.getDefaultMessage());
+                errorMessage.append(";");
+            }
+            errorMessage.replace(errorMessage.lastIndexOf(";"),errorMessage.lastIndexOf(";")+1,"。");
+            return errorMessage.toString();
+        }else {
+            return "";
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
