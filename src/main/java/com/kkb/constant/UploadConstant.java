eda5135b2a3de70fe9c5010d4a394df6d4499e4e
hanweiwu <hanwei_wu@163.com>
2021-11-06 18:54:21 +0800

    细节更新

--- src/main/java/com/kkb/constant/UploadConstant.java
+++ src/main/java/com/kkb/constant/UploadConstant.java
@@ -9,11 +9,11 @@ package com.kkb.constant;
 public class UploadConstant {
 
     /* 图片上传位置 （注意Linux和Windows上的目录结构不同）  */
-    public static String UPLOAD_FILE_PATH = "D:/shandong-hospita/upload";
+    public static String UPLOAD_FILE_PATH;
     /* 支持的图片类型 */
     public static String UPLOAD_FILE_TYPE;
     /* 上传资源映射路径 前缀*/
-    public static String RESOURCE_PREFIX = "/shandong-hospita/uploads";
+    public static String RESOURCE_PREFIX;
 
     // springmvc自动注入属性
     public UploadConstant(String uploadFilePath, String resourcePrefix, String upLoadFileType) {
